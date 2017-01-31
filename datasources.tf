812d3a791789ee91ad9dead371258619399545f0
James Hollins <james.hollins@bashton.com>
2017-01-31 13:44:33 +0000

    Removed specific values from userdata, now gets cert pfx password from parameter store

--- datasources.tf
+++ datasources.tf
@@ -26,13 +26,17 @@ data "template_file" "rdgw_userdata" {
 
   vars {
 
-    region          = "${var.aws_region}"
-    ad_user         = "${lookup(var.admin_users,var.ad_type)}"
-    dns_servers     = "${element(var.ads_dns,0)},${element(var.ads_dns,1)}"
-    local_password  = "${var.local_password}"
-    domain_password = "${var.domain_password}"
-    domain_name     = "${var.domain_name}"
-    route53_zone_id = "${var.route53_zone_id}"
+    region                = "${var.aws_region}"
+    ad_user               = "${lookup(var.admin_users,var.ad_type)}"
+    dns_servers           = "${element(var.ads_dns,0)},${element(var.ads_dns,1)}"
+    local_password        = "${var.local_password}"
+    domain_password       = "${var.domain_password}"
+    domain_name           = "${var.domain_name}"
+    route53_zone_id       = "${var.route53_zone_id}"
+    certificate_bucket_id = "${var.certificate_bucket_id}"
+    certificate_object_id = "${var.certificate_object_id}"
+    ssm_param_value       = "${var.ssm_param_value}"
+
   }
 
 }
