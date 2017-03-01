8607ede6477d39e070549bb288069b74e89c193b
James Hollins <james.hollins@bashton.com>
2017-03-01 11:49:07 +0000

    Upated readme and modified name of asg

--- asg.tf
+++ asg.tf
@@ -1,21 +1,20 @@
-
 ## builds rdp jump servers
 
 module "asg" {
   source = "../tf-aws-asg"
 
-  name                  = "${var.customer}-${var.envtype}-${var.service}"
-  envname               = "${var.envname}"
-  service               = "${var.envtype}"
+  name    = "${var.customer}-${var.service}"
+  envname = "${var.envname}"
+  service = "${var.envtype}"
 
-  ami_id                = "${data.aws_ami.windows.id}"
-  instance_type         = "${var.instance_type}"
-  iam_instance_profile  = "${module.iam_instance_profile.profile_id}"
-  key_name              = "${var.key_name}"
-  security_groups       = ["${aws_security_group.rdgw_external.id}","${var.ads_sg}"]
-  user_data             = "<powershell>${data.template_file.rdgw_userdata.rendered}</powershell><persist>true</persist>"
-  subnets               = "${var.public_subnets}"
-  availability_zones    = "${var.azs}"
-  asg_min               = "${length(var.azs)}"
-  asg_max               = "${length(var.azs)}"
+  ami_id               = "${data.aws_ami.windows.id}"
+  instance_type        = "${var.instance_type}"
+  iam_instance_profile = "${module.iam_instance_profile.profile_id}"
+  key_name             = "${var.key_name}"
+  security_groups      = ["${aws_security_group.rdgw_external.id}", "${var.ads_sg}"]
+  user_data            = "<powershell>${data.template_file.rdgw_userdata.rendered}</powershell><persist>true</persist>"
+  subnets              = "${var.public_subnets}"
+  availability_zones   = "${var.azs}"
+  asg_min              = "${length(var.azs)}"
+  asg_max              = "${length(var.azs)}"
 }
