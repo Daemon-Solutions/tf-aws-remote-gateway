71d7492bb60aca2d91cfcacdccb81674c6155363
Jamie French <jamie.french@bashton.com>
2017-06-03 11:03:24 +0100

    added parameter associate_public_ip_address

--- asg.tf
+++ asg.tf
@@ -3,19 +3,17 @@
 module "asg" {
   source = "../tf-aws-asg"
 
-  name                  = "${var.service}-${var.envtype}"
-  envname               = "${var.envname}"
-  service               = "${var.envtype}"
-
-  ami_id                = "${data.aws_ami.windows.id}"
-  instance_type         = "${var.instance_type}"
-  iam_instance_profile  = "${module.iam_instance_profile.profile_id}"
-  key_name              = "${var.key_name}"
-  security_groups       = ["${aws_security_group.rdgw_external.id}","${aws_security_group.rdgw_internal.id}","${var.ads_sg}"]
-  user_data             = "<powershell>${var.domain_connect_userdata}</powershell><persist>true</persist>"
-
-  subnets               = "${var.public_subnets}"
-  
-  min               = "${length(var.azs)}"
-  max               = "${length(var.azs)}"
+  name                        = "${var.service}-${var.envtype}"
+  envname                     = "${var.envname}"
+  service                     = "${var.envtype}"
+  ami_id                      = "${data.aws_ami.windows.id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${module.iam_instance_profile.profile_id}"
+  key_name                    = "${var.key_name}"
+  security_groups             = ["${aws_security_group.rdgw_external.id}", "${aws_security_group.rdgw_internal.id}", "${var.ads_sg}"]
+  user_data                   = "<powershell>${var.domain_connect_userdata}</powershell><persist>true</persist>"
+  associate_public_ip_address = true
+  subnets                     = "${var.public_subnets}"
+  min                         = "${length(var.azs)}"
+  max                         = "${length(var.azs)}"
 }
