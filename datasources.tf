cbf028568e8ad83b7ff00be9965417b389c4dc9d
Liam Bennett <liam@bashton.com>
2017-07-04 10:59:15 +0100

    ensure datasource looksup only amazon base image

--- datasources.tf
+++ datasources.tf
@@ -1,11 +1,17 @@
 #retrieve the latest ami for 2008R2/2012R2/2016
 
 data "aws_ami" "windows" {
- most_recent = true
- filter {
-   name = "name"
-   values = ["${lookup(var.windows_ami_names,var.windows_ver)}"]
-    }
+  most_recent = true
+  
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name = "name"
+    values = ["${lookup(var.windows_ami_names,var.windows_ver)}"]
+  }
  }
 
  data "aws_vpc" "cidr" {
