89c272159d8cb91162fcf4b51b767ed23e383e0d
James Hollins <james.hollins@bashton.com>
2017-07-17 13:45:37 +0100

    updated vars and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,23 @@
-
 ## IAM Outputs
 output "rdgw_iam_profile_id" {
-  value = "${module.iam_instance_profile.profile_id}"
+  value = "${aws_iam_instance_profile.instance_profile.profile_id}"
 }
 
 ## Launch Config Outputs
 output "launch_config_id" {
-  value = "${module.asg.launch_config_id}"
+  value = "${aws_launch_configuration.lc.launch_config_id}"
 }
 
 ## Autoscaling Group Outputs
 output "asg_id" {
-  value = "${module.asg.asg_id}"
+  value = "${aws_autoscaling_group.asg.asg_id}"
 }
 
 output "asg_name" {
-  value = "${module.asg.asg_name}"
+  value = "${aws_autoscaling_group.asg.asg_name}"
 }
 
 ## Security Group Outputs
-output "rdgw_external_sg_id" {
-  value = "${aws_security_group.rdgw_external.id}"
+output "rdgw_sg_id" {
+  value = "${aws_security_group.rdgw.id}"
 }
