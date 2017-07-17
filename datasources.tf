4b20346c345b275caf6ecdaf9b90770c36aefa2d
James Hollins <james.hollins@bashton.com>
2017-07-17 13:43:30 +0100

    updated name of userdata var and datasource

--- datasources.tf
+++ datasources.tf
@@ -1,31 +1,30 @@
 #retrieve the latest ami for 2008R2/2012R2/2016
 
 data "aws_ami" "windows" {
- most_recent = true
- filter {
-   name = "name"
-   values = ["${lookup(var.windows_ami_names,var.windows_ver)}"]
-    }
- }
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["${lookup(var.windows_ami_names,var.windows_ver)}"]
+  }
+}
 
 data "aws_vpc" "cidr" {
-   id = "${data.aws_subnet.vpc.vpc_id}"
- }
+  id = "${data.aws_subnet.vpc.vpc_id}"
+}
 
 data "aws_subnet" "vpc" {
   id = "${element(var.public_subnets,0)}"
 }
 
 data "aws_region" "current" {
-   current = true
+  current = true
 }
 
-data "template_file" "rdgw_userdata" {
-
-  template = "${file("${path.module}/include/rdgw_userdata.tmpl")}"
+data "template_file" "userdata" {
+  template = "${file("${path.module}/include/userdata.tmpl")}"
 
   vars {
-
     region                = "${var.aws_region}"
     ad_user               = "${lookup(var.admin_users,var.ad_type)}"
     dns_servers           = "${element(var.ads_dns,0)},${element(var.ads_dns,1)}"
