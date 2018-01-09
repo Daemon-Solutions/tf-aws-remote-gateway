2a33c98c6583eb522427a25c341fbf39b2b8693e
Jamie French <jamie.french@claranet.uk>
2018-01-09 12:33:58 +0000

    profile updated to allow setting tags

--- profile.tf
+++ profile.tf
@@ -3,10 +3,11 @@
 module "iam_instance_profile" {
   source = "../tf-aws-iam-instance-profile"
 
-  name         = "${var.customer}-${var.service}"
-  ec2_describe = "1"
-  ec2_attach   = "1"
-  s3_readonly  = "1"
-  r53_update   = "1"
-  ssm_managed  = "1"
+  name            = "${var.customer}-${var.service}"
+  ec2_describe    = "1"
+  ec2_attach      = "1"
+  s3_readonly     = "1"
+  r53_update      = "1"
+  ssm_managed     = "1"
+  ec2_write_tags  = "1"
 }
