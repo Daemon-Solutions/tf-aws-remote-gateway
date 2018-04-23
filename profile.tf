a4ff178271ccbf334c8965d49aa1bbbb83ce8aa1
Jamie French <jamie.french@claranet.uk>
2018-04-23 17:52:37 +0100

    removed deprecated s3_readonly from profile

--- profile.tf
+++ profile.tf
@@ -1,13 +1,10 @@
-
-
 module "iam_instance_profile" {
   source = "../tf-aws-iam-instance-profile"
 
-  name            = "${var.customer}-${var.service}"
-  ec2_describe    = "1"
-  ec2_attach      = "1"
-  s3_readonly     = "1"
-  r53_update      = "1"
-  ssm_managed     = "1"
-  ec2_write_tags  = "1"
+  name           = "${var.customer}-${var.service}"
+  ec2_describe   = "1"
+  ec2_attach     = "1"
+  r53_update     = "1"
+  ssm_managed    = "1"
+  ec2_write_tags = "1"
 }
